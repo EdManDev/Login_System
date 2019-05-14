420006fe9e4ed59400e5932fb6bdb6f967dccdce
EdManDev <ed.manigat@gmail.com>
2019-05-14 13:05:41 -0700

    update

--- index.php
+++ index.php
@@ -1,9 +1,40 @@
 <?php 
   include_once 'header.php';
 ?>
+<!DOCTYPE html>
+<html lang="en">
 
-<h2>home here</h2>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+  <link rel="stylesheet" href="style.css">
+</head>
 
-<?php 
+<body>
+  <header>
+    <nav>
+      <div class="main-wrapper">
+        <ul>
+          <li><a href="index.php">home</a></li>
+        </ul>
+        <div class="nav-login">
+          <form action="POST">
+            <input type="text" name="uid" placeholder="/e-mail">
+            <input type="password" name="pwd" placeholder="password">
+            <button type="submit" name="submit">login</button>
+          </form>
+          <a href="signup.php">Sign up</a>
+        </div>
+      </div>
+    </nav>
+  </header>
+  <section>
+  </section>
+</body>
+
+</html>
+<h2>home here</h2> <?php 
   include_once 'footer.php';
 ?>
