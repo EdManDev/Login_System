6d936204483e308ccc3a90ecd92dbf39956490d0
EdManDev <ed.manigat@gmail.com>
2019-05-14 12:53:01 -0400

    yes

--- index.php
+++ index.php
@@ -1,20 +1,9 @@
-<html lang="en">
+<?php 
+include_once 'header.php';
+?>
 
-<head>
-  <meta charset="UTF-8">
-  <title>Login System</title>
-</head>
+<h2>home</h2>
 
-<body>
-  <ul>
-    <li><a href="index.php">Home</a></li>
-  </ul>
-  <form>
-    <input type="text" name="uid" placeholder="username/e-mail">
-    <input type="password" name="pwd" placeholder="password">
-    <button type="submit" name="submit">Login</button>
-  </form>
-  <a href="signup.php">Sign Up</a>
-</body>
-
-</html>
+<?php 
+include_once 'footer.php';
+?>
