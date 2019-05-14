077e202dabe2080bd3965b929838ac937c8c45ee
EdManDev <ed.manigat@gmail.com>
2019-05-14 08:21:23 -0400

    update

--- index.php
+++ index.php
@@ -1,12 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Login System</title>
 </head>
+
 <body>
-  
+  <form>
+    <input type="text" name="uid" placeholder="username/e-mail">
+    <input type="password" name="pwd" placeholder="password">
+    <button type="submit" name="submit">Login</button>
+  </form>
 </body>
+
 </html>
